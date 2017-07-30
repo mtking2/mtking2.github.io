87a6130829871833ae96102b9fe885ebb4846787
Michael King <mtking1123@gmail.com>
2017-07-30 16:49:09 -0400

    add goodreads data + section. fix instagram - more pictures.

--- src/getStuff.js
+++ src/getStuff.js
@@ -50,16 +50,65 @@ var requestInstagramData = function(cb) {
         // .sortBy(function(el, index) {
         //   return -1 * el.likes.count + index * 1.5
         // })
-        .filter({ type: 'image' })
-        .take(8)
+        // .filter({ type: 'image',})
+        .take(12)
         .value()
 
       cb(null, instagramGeneralData)
     })
 }
 
+var requestGoodreadsData = function(cb) {
+  console.log('REQUEST: GOODREADS')
+  axios
+    .get('https://www.goodreads.com/review/list/69517269.xml?key=' + process.env.GOODREADS_KEY + '&v=2')
+    .then(function(res) {
+      console.log('RESPONSE: GOODREADS')
+
+      let goodReadsXml = '\n' + res.data
+      parseString(goodReadsXml, function(parseErr, result) {
+        if (parseErr) {
+          return cb(parseErr)
+        }
+
+        var parsed = _.chain(result.GoodreadsResponse.reviews[0].review)
+          .map(review => {
+            var book = review.book[0]
+
+            return {
+              title: book.title[0],
+              author: _.get(book, 'authors[0].author[0].name'),
+              url: book.link[0],
+              thumbnail: book.large_image_url[0] || book.image_url[0] || book.small_image_url[0],
+              myRating: review.rating[0] ? parseInt(review.rating[0], 10) : null,
+              status: _.get(review, 'shelves[0].shelf[0].$.name'),
+              date: new Date(review.date_updated[0])
+            }
+          })
+          .sortBy(book => {
+            return -book.date.getTime()
+          })
+          .sortBy(book => {
+            const statusValue = {
+              'currently-reading': 1,
+              read: 2
+            }
+
+            return statusValue[book.status] || 3
+          })
+          .take(5)
+          .value()
+
+        cb(null, parsed)
+      })
+    })
+    .catch(function(e) {
+      console.error(e)
+    })
+}
+
 module.exports = function(cb) {
-  async.parallel([requestGithubData, requestInstagramData], function(
+  async.parallel([requestGithubData, requestInstagramData, requestGoodreadsData], function(
     err,
     results
   ) {
@@ -67,7 +116,8 @@ module.exports = function(cb) {
 
     cb(null, {
       githubData: results[0],
-      instagramData: results[1]
+      instagramData: results[1],
+      goodreadsData: results[2]
     })
   })
 }
